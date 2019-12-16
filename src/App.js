2c9e5278725c8c9d69da4f240a8910e55bda57c7
Mark Leo <ratrateroo@gmail.com>
2019-12-16 22:48:19 +0800

    covered components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// class App extends React.Component {
+//   render() {
+//     return <h1 title="This works!">A React App!</h1>;
+//   }
+// }
+
 const App = () => {
   //return React.createElement('h1', {title: 'This works!'}, 'A React App!';
   return <h1 title="This works!">A React App!</h1>;
