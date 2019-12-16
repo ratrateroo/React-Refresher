3c5af29992c3e3cacc19664d0631f98faeeb549b
Mark Leo <ratrateroo@gmail.com>
2019-12-16 23:30:25 +0800

    rendered list of data

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -5,7 +5,11 @@ import './GoalList.css';
 const GoalList = props => {
     return (
         <ul className="goal-list">
-            
+            {
+                props.goals.map((goal) => {
+                    return <li key={goal.id}>{goal.text}</li>
+                })
+            }
         </ul>
         );    
 };
